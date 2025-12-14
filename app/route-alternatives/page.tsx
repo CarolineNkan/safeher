e82c766f093411e6223ac6bdee7070861b3c3387
CarolineNkan <145355980+CarolineNkan@users.noreply.github.com>
2025-12-14 06:22:34 -0500

    Update page.tsx

--- app/route-alternatives/page.tsx
+++ app/route-alternatives/page.tsx
@@ -1,5 +1,5 @@
-"use client";
 export const dynamic = "force-dynamic";
+"use client";
 import { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import RouteAlternativesMap from "../../components/RouteAlternativesMap";
